db884e06ea4377a849d4c35067a74f6f0cbaeccd
Aryan Khorram <aryankhorram@Aryans-MacBook-Pro.local>
2020-09-16 15:14:43 -0700

    added procfile

---
+++ Procfile.h
@@ -0,0 +1,16 @@
+//
+//  Procfile.h
+//  
+//
+//  Created by Aryan Khorram on 9/16/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Procfile : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ app.h
@@ -0,0 +1,16 @@
+//
+//  app.h
+//  
+//
+//  Created by Aryan Khorram on 9/16/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface app : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
